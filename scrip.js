ca053e331b37c451250ed3ea76c5a9089c37c53f
EderDSouza <velasquessouza@gmail.com>
2022-10-15 14:20:24 -0300

    trabalhando com Eventos

--- scrip.js
+++ scrip.js
@@ -1,9 +1,22 @@
 
 function btnMenu(){
-    if(menuNav.style.display=="block"){
+    if(menuNav.style.display=="block"|| cadastroNew.style.display=="block"){
         menuNav.style.display="none"
     }else{
         menuNav.style.display="block"
     }
    
 }
+function cadastroBtn(){
+    if(cadastroNew.style.display=="block"){
+        cadastroNew.style.display="none"
+    }else{
+        cadastroNew.style.display="block"
+    }
+}
+
+var clicarMenu =document.querySelector('#menuBtn');
+var clicarCadastro = document.querySelector('#btnCadastrar');
+
+clicarMenu.addEventListener('click',btnMenu)
+clicarCadastro.addEventListener('click',cadastroBtn)
